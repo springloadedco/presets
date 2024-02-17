a2e4f2a14e1b6f5c63170089bffe05684cdf75d0
Owen Conti <owen@springloaded.co>
2024-02-17 14:04:39 -0700

    feat: Add preset for installing php-cs-fixer

---
+++ php-cs-fixer/README.md
@@ -0,0 +1,11 @@
+# Springloaded PHP CS Fixer
+
+This preset will install and configure PHP CS Fixer in your project.
+
+## Usage
+
+Run the following from the root of your project:
+
+```
+preset apply springloadedco/presets --path php-cs-fixer
+```
---
+++ php-cs-fixer/preset.ts
@@ -0,0 +1,12 @@
+export default definePreset({
+	name: 'php-cs-fixer',
+	options: {},
+	handler: async() => {
+		await extractTemplates(),
+		await installPackages({
+			for: 'php',
+			packages: ['friendsofphp/php-cs-fixer'],
+			dev: true,
+		})
+	},
+})
