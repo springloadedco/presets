9502a7995f3ba841ae9035ed8bfc3a0930ee5261
Owen Conti <owen@springloaded.co>
2024-02-17 15:09:41 -0700

    Add preset for installing eslint/prettier

---
+++ eslint-vue/README.md
@@ -0,0 +1,12 @@
+# ESLint and Prettier Preset
+
+This preset will install and configure ESLint and Prettier in your project.
+
+## Usage
+
+Run the following from the root of your project:
+
+```
+preset apply springloadedco/presets --path eslint-vue
+```
+
---
+++ eslint-vue/preset.ts
@@ -0,0 +1,38 @@
+export default definePreset({
+	name: 'eslint-vue',
+	handler: async() => {
+		await extractTemplates(),
+
+		await editFiles({
+			title: 'Add lint scripts',
+			files: 'package.json',
+			operations: {
+				type: 'edit-json',
+				merge: {
+					scripts: {
+						"lint": "./node_modules/.bin/eslint --ext .ts,.vue resources/",
+						"lint-fix": "./node_modules/.bin/eslint --fix --ext .ts,.vue resources/"
+					},
+				},
+			},
+		}),
+
+		await installPackages({
+			title: 'Add NPM dependencies',
+			for: 'node',
+			dev: true,
+			packages: [
+				'@typescript-eslint/eslint-plugin',
+				'@typescript-eslint/parser',
+				'@vue/eslint-config-prettier',
+				'@vue/eslint-config-typescript',
+				'eslint',
+				'eslint-plugin-no-relative-import-paths',
+				'eslint-plugin-unused-imports',
+				'prettier',
+				'prettier-plugin-tailwindcss',
+				'typescript'
+			]
+		})
+	},
+})
