61bcedc8b35b3a42c027b225d90df6fcb639a15a
Owen Conti <owen@springloaded.co>
2024-04-22 21:01:18 -0600

    fix(PRE-7): Add fix command with php-cs-fix preset, add php-cs-fixer cache to .gitignore file

--- php-cs-fixer/preset.ts
+++ php-cs-fixer/preset.ts
@@ -10,5 +10,26 @@ export default definePreset({
       packages: ['friendsofphp/php-cs-fixer:*'],
       dev: true
     });
+    await editFiles({
+      files: ['composer.json'],
+      operations: {
+        type: 'edit-json',
+        merge: {
+          scripts: {
+            fix: [
+              './vendor/bin/php-cs-fixer fix --config .php-cs-fixer.dist.php app database config'
+            ]
+          }
+        }
+      }
+    });
+    await editFiles({
+      files: ['.gitignore'],
+      operations: {
+        type: 'add-line',
+        position: 'append',
+        lines: '.php-cs-fixer.cache'
+      }
+    });
   }
 });
